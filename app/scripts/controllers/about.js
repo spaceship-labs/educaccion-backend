bf6b3ad7c9b0dea19f54a5ab851f960b3147c2ed
el-sonny <sonny@spaceshiplabs.com>
2015-09-11 12:02:53 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name educaccionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the educaccionApp
+ */
+angular.module('educaccionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name educaccionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the educaccionApp
+ */
+angular.module('educaccionApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
